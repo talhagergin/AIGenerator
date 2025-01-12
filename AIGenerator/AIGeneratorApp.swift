9db757a5c52078ddf62123bad535fe80d1943ec9
Talha Gergin <talhagergin6@gmail.com>
2025-01-12 15:14:32 +0300

    Initial Commit

---
+++ AIGenerator/AIGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIGeneratorApp.swift
+//  AIGenerator
+//
+//  Created by Talha Gergin on 12.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct AIGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
